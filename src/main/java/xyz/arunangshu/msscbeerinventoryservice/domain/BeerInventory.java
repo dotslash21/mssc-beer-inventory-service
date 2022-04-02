45807352b2ea389527fb3bdc3b646b1000e246a0
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-04-02 14:40:28 +0530

    Added domain entity classes, closes #2

---
+++ src/main/java/xyz/arunangshu/msscbeerinventoryservice/domain/BeerInventory.java
@@ -0,0 +1,50 @@
+package xyz.arunangshu.msscbeerinventoryservice.domain;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+public class BeerInventory extends BaseEntity {
+
+    @Builder
+    public BeerInventory(
+            UUID id,
+            Long version,
+            Timestamp createdDate,
+            Timestamp lastModifiedDate,
+            UUID beerId,
+            String upc,
+            Integer quantityOnHand) {
+
+        super(id, version, createdDate, lastModifiedDate);
+        this.beerId = beerId;
+        this.upc = upc;
+        this.quantityOnHand = quantityOnHand;
+    }
+
+    private UUID beerId;
+    private String upc;
+    private Integer quantityOnHand = 0;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        BeerInventory that = (BeerInventory) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
